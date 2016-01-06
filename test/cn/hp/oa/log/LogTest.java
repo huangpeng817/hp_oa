fd51fbac100917225165d3038ce2c47355992cca
huangpeng817 <huangpeng817@qq.com>
2016-01-06 16:46:44 +0800

    配置log4j，测试日志功能，使cn.hp.oa包下的日志未debug级别(出错最详细)，其他的资源日志级别为warn

---
+++ test/cn/hp/oa/log/LogTest.java
@@ -0,0 +1,19 @@
+package cn.hp.oa.log;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+public class LogTest {
+
+	private Log log = LogFactory.getLog(this.getClass());
+	
+	@Test
+	public void testLog() throws Exception {
+		log.debug("debug...");
+		log.info("info...");
+		log.warn("warn...");
+		log.error("error...");
+		log.fatal("fatal...");
+	}
+}
